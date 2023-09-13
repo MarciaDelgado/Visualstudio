f7baf40806ac10e44d235c7cb24595907d48c5de
TEC\MDE <Marcia.Delgado@tecalliance.net>
2023-09-13 09:21:33 +0200

    C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gebe mir deinen Namen");
+            
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, !" + name );
+        }
+    }
+}
