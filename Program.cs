a1aad77550fbb182c8c09edfd40430ce5e58382b
TEC\MDE <Marcia.Delgado@tecalliance.net>
2023-09-13 09:52:31 +0200

    age

--- Program.cs
+++ Program.cs
@@ -7,9 +7,15 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Gebe mir deinen Namen");
-            
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, !" + name );
+             string name = Console.ReadLine();
+
+            Console.WriteLine("Sage mir wie alt du bist");
+            string age = Console.ReadLine();
+
+            int alter = Convert.ToInt16(age);
+            int alterInZehn = alter + 10;
+
+            Console.WriteLine("Hello, " + name + " Dein Alter ist" + age + " In zehn Jahren bist du "+ alterInZehn ) ;
         }
     }
 }
