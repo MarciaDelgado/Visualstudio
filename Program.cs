5c893fe6f539403efc37e84148efb1e1e9b11d21
TEC\MDE <Marcia.Delgado@tecalliance.net>
2023-09-13 11:48:00 +0200

    x y z

--- Program.cs
+++ Program.cs
@@ -7,45 +7,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            string myText = "Hello World";
-            Console.WriteLine(myText);
+            int x, y, z;
+            x = y = z = 50;
+            Console.WriteLine(x + y + z);
 
-            int myNum = 9;
-            Console.WriteLine(myNum);
-
-            double kommazahl =8.99;
-            Console.WriteLine(kommazahl);
-
-            /*float weiterFliesKommazahl = 26.333333673F;
-            Console.WriteLine(weiterFliesKommazahl);
-
-            decimal fliesKomma = 26.5M;
-            Console.WriteLine(fliesKomma);*/
-
-
-            bool wahrheitswert = false ;
-            Console.WriteLine(wahrheitswert);
-           /* bool anders = false ;
-            Console.WriteLine(anders);*/
-
-            char buchstabe =(char)65 ;
-            Console.WriteLine(buchstabe);
-            
-            /*long langezahl = 123456789123456789;
-            Console.WriteLine(langezahl);
-
-            uint positiveZahlen = 123;
-            Console.WriteLine(positiveZahlen);
-
-            string[] myArray = new string [10];
-            myArray[0] = "erster Wert";
-            myArray[9] = "letzer Wert";
-
-            List<string> myList = new List<string>();
-            myList.Add("Marcia");
-            myList.Add("Benjamin");
-            Console.WriteLine(myList[0]);
-            Console.WriteLine(myList[1]);*/
 
         }
     }
