78a3006465abe10def9a5c60b7acf69590793b61
TEC\MDE <Marcia.Delgado@tecalliance.net>
2023-09-15 09:03:10 +0200

    Switch + met

--- Program.cs
+++ Program.cs
@@ -7,37 +7,56 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args)
-        {          
-            int zahl1=10;
-            int zahl2=2;
-           
-           Console.WriteLine("Wählen ein Zeichnen aus");
-           string zeichnen=Console.ReadLine();
+        {           
+            int zahl1 = 5;
+            int zahl2 = 10;
+            Console.WriteLine("Wählen ein Zeichnen aus");
+            string zeichnen=Console.ReadLine();
+            switch(zeichnen)
+            {            
+                case "+":
+                    Addition(zahl1, zahl2);
+                    break;
 
-           switch(zeichnen)
-           {
-            case "+":
-             Console.WriteLine(zahl1 + zahl2);
-             break;
+                case "-":
+                    Substract(zahl1 ,zahl2);
+                    break;
 
-            case "-":
-             Console.WriteLine(zahl1 - zahl2);
-             break;
+                case "/":
+                    Division(zahl1 ,zahl2);
+                    break;
 
-            case "/":
-             Console.WriteLine(zahl1 / zahl2);
-             break;
+                case "*":
+                    Multiplication(zahl1 , zahl2);
+                    break;   
 
-            case "*":
-             Console.WriteLine(zahl1 * zahl2);
-             break;   
-
-            default:
-            Console.WriteLine("Ich kenne diesen Operator nicht ");
-            break;
-
+                default:
+                    Console.WriteLine("Ich kenne diesen Operator nicht ");
+                    break;
            }
-           
-           }
+        }
+        
+        static void  Addition(int zahl1, int zahl2)
+        {
+            Console.WriteLine(zahl1+zahl2);
+        }
+          
+         static void Substract(int zahl1, int zahl2) 
+         {
+            Console.WriteLine(zahl1-zahl2);
+         }
+         
+          static void Division(int zahl1, int zahl2) 
+         {
+            Console.WriteLine(zahl1/zahl2);
+         }
+         
+
+          static void Multiplication(int zahl1, int zahl2) 
+         {
+            Console.WriteLine(zahl1*zahl2);
+         }
+
+
         }
     }
