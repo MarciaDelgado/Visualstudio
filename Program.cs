ff6961d320ebcfb4246ad1eea725368cd764e246
TEC\MDE <Marcia.Delgado@tecalliance.net>
2023-09-19 12:47:31 +0200

    Methodeennn

--- Program.cs
+++ Program.cs
@@ -1,24 +1,53 @@
-﻿using System.Globalization;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks.Dataflow;
-using Microsoft.VisualBasic;
+﻿using System.ComponentModel;
+using System.Globalization;
 
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
-        {           
-            Console.WriteLine("Geben Sie eine Zahl ein ");
-            int zahl1=Convert.ToInt16(Console.ReadLine());
+        {
+            Console.WriteLine("Wie schnell fährt das Auto?");
+            int nmr=Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Geben Sie noch eine Zahl ein ");
-            int zahl2=Convert.ToInt16(Console.ReadLine());
+            string ergebnis=Fahren(nmr);
+            Console.WriteLine($"Das Auto {ergebnis}");
+        }
 
-            int ergebnis=zahl1%zahl2;
-            Console.WriteLine($"Die Division von {zahl1} und {zahl2} erzeugt den Rest {ergebnis} " );
+        static string Fahren(int geschwindigkeit)
+        {
+       
+            if(geschwindigkeit==0)
+            {
+                return " steht";
+            }
+
+            else if (geschwindigkeit>0&&geschwindigkeit<=10)
+            {
+              return   "schleicht" ;
             
+            }    
+
+            else if(geschwindigkeit>10&&geschwindigkeit<=50)
+            {
+                return "fährt langsam";
+            }
+            
+            else if(geschwindigkeit>50&&geschwindigkeit<=100)
+            {
+                return "fährt schnell";
+            }
+
+            else if(geschwindigkeit>100)
+            {
+                return  "rast";
+            }
+
+            else
+            { 
+                return "Error";
+            }
+
         }
     }
 }
